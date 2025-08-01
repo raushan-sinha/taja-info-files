4aa81355a9f7c2eef9db23441a53ae153668af55
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-08-01 08:36:29 +0530

    In navbar.jsx created <main> tag and Home Page created in different file.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,33 +15,35 @@ const Navbar = () => {
     const toggleMenu = () => setMenuOpen(prev => !prev);
 
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.logo}>{navbarTitle}</div>
+        <main>
+            <nav className={styles.navbar}>
+                <div className={styles.logo}>{navbarTitle}</div>
 
-            {/* Hamburger / Close Icon */}
-            <div className={styles.hamburger} onClick={toggleMenu}>
-                {menuOpen ? (
-                    <span className={styles.closeIcon}>&times;</span>
-                ) : (
-                    <>
-                        <span className={styles.bar}></span>
-                        <span className={styles.bar}></span>
-                        <span className={styles.bar}></span>
-                    </>
-                )}
-            </div>
+                {/* Hamburger / Close Icon */}
+                <div className={styles.hamburger} onClick={toggleMenu}>
+                    {menuOpen ? (
+                        <span className={styles.closeIcon}>&times;</span>
+                    ) : (
+                        <>
+                            <span className={styles.bar}></span>
+                            <span className={styles.bar}></span>
+                            <span className={styles.bar}></span>
+                        </>
+                    )}
+                </div>
 
-            {/* Nav Links */}
-            <ul className={`${styles.navlinks} ${menuOpen ? styles.active : ''}`}>
-                {navLinks.map((link, idx) => (
-                    <li key={idx}>
-                        <Link href={link.url} onClick={() => setMenuOpen(false)}>
-                            {link.title}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+                {/* Nav Links */}
+                <ul className={`${styles.navlinks} ${menuOpen ? styles.active : ''}`}>
+                    {navLinks.map((link, idx) => (
+                        <li key={idx}>
+                            <Link href={link.url} onClick={() => setMenuOpen(false)}>
+                                {link.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </main>
     );
 };
 
